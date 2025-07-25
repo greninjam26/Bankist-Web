e6521c312827efed8f2575ab0697bb9fd6517943
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-07-25 15:39:41 -0400

    esc modal

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 "use strict";
 
 // DOM selection
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--open-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--open-modal");
 
 // Modal Window
 const openModal = function () {
     modal.classList.remove("hidden");
     overlay.classList.remove("hidden");
-}
+};
 const closeModal = function () {
     modal.classList.add("hidden");
     overlay.classList.add("hidden");
-}
+};
 
 btnsOpenModal.forEach(btn => btn.addEventListener("click", openModal));
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+});
