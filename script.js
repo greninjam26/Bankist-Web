e2e42d6e41d8ddac42a19877fdea5bd3157471cc
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-07-26 11:23:34 -0400

    add some scrolling

--- script.js
+++ script.js
@@ -3,8 +3,13 @@
 // DOM selection
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
+const section1 = document.querySelector("#section--1");
+const section2 = document.querySelector("#section--2");
+const section3 = document.querySelector("#section--3");
+
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--open-modal");
+const btnScroll = document.querySelector(".btn--scroll-to");
 
 // Modal Window
 const openModal = function (e) {
@@ -24,4 +29,19 @@ document.addEventListener("keydown", function (e) {
     if (e.key === "Escape" && !modal.classList.contains("hidden")) {
         closeModal();
     }
+});
+
+// scroll to section
+btnScroll.addEventListener("click", function () {
+    // scroll to section
+    // old way
+    // the location of the section 1
+    // const s1coords = section1.getBoundingClientRect();
+    // window.scrollTo({
+    //     left: s1coords.left + window.scrollX,
+    //     top: s1coords.top + window.scrollY,
+    //     behavior: "smooth",
+    // });
+    // new way
+    section1.scrollIntoView({ behavior: "smooth" });
 });
